2e80d3282f00a1f0c7902d42f5e4b705431028ac
wanglin@10.21.4.68 2007 <wanglin199701@gmail.com>
2026-04-25 11:50:44 +0000

    modified:   onerec_v2/DRAGON/main.py

--- onerec_v2/DRAGON/main.py
+++ onerec_v2/DRAGON/main.py
@@ -24,27 +24,7 @@ if __name__ == '__main__':
     parser.add_argument("--use_residual", type=ast.literal_eval, default=True, help="whether to use residual connection (True/False)")
     parser.add_argument("--gpu_id", type=int, default=0, help="gpu id")
 
-    # List 与 Float 参数
-    # parser.add_argument("--lr", type=ast.literal_eval, default=[0.0005], help='learning rate list, e.g. "[0.0005, 0.001]"')
-    # parser.add_argument("--mix_bpr_weight_loss", type=ast.literal_eval, default=[0.1], help='mix BPR loss weight list, e.g. "[0.1, 0.2]"')
-    # parser.add_argument("--dragon_bpr_weight", type=ast.literal_eval, default=[0.1], help='dragon BPR weight list, e.g. "[0.1, 0.2]"')
-    # parser.add_argument("--align_weight_loss", type=ast.literal_eval, default=[0.1], help='alignment loss weight list, e.g. "[0.1, 0.2]"')
-    # parser.add_argument("--diver_weight_loss", type=ast.literal_eval, default=[0.1], help='diversity loss weight list, e.g. "[0.1, 0.2]"')
-
     args = parser.parse_args()
-
-    # # 从命令行动态构建 config_dict
-    # config_dict = {
-    #     'use_homogeneity': args.use_homogeneity,
-    #     'use_diversity': args.use_diversity,
-    #     'use_align_loss': args.use_align_loss,
-    #     'use_residual': args.use_residual,
-    #     'learning_rate': args.lr,
-    #     'mix_bpr_weight_loss': args.mix_bpr_weight_loss,
-    #     'dragon_bpr_weight': args.dragon_bpr_weight,
-    #     'align_weight_loss': args.align_weight_loss,
-    #     'diver_weight_loss': args.diver_weight_loss,
-    # }
 
     if args.dataset == 'baby_sparse':
         config_dict = {
@@ -86,9 +66,6 @@ if __name__ == '__main__':
             'seed': [999],
         }
 
-    # config_dict = {
-    #     'gpu_id': args.gpu_id,
-    # }
 
     args, _ = parser.parse_known_args()
     print("config_dict", config_dict)
